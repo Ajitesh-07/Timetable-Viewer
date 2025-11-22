30f0b6a1b31d8981ba290e52af8bf92fc50e6f17
Ajitesh <ajitesh7011@gmail.com>
2025-11-22 10:29:14 +0530

    fixed timing

--- src/app/endsem/api/route.ts
+++ src/app/endsem/api/route.ts
@@ -65,7 +65,7 @@ export async function GET(request: NextRequest) {
             results.push({
                 date: slot.date,
                 day: toLowercaseExceptFirst(slot.day),
-                time: (slot.shift == 'Morning') ? "10:30 - 12:30" : "15:30 - 17:30",
+                time: (slot.shift == 'Morning') ? "10:30 - 12:30" : "15:00 - 18:00",
                 course: slot.coursecode,
                 location: slot.roomno
             });
